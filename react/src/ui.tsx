5df47027ed37fbe44c52040ce3a6c7a22219d338
Leonard Bogdonoff <lkbgift@gmail.com>
2020-05-11 20:57:58 -0400

    Update the React example to use functions.
    Removing the class example to modernize the React best practices.

--- react/src/ui.tsx
+++ react/src/ui.tsx
@@ -4,20 +4,20 @@ import './ui.css'
 
 declare function require(path: string): any
 
-class App extends React.Component {
-  textbox: HTMLInputElement
+const App = () => {
+  let textbox: HTMLInputElement;
 
-  countRef = (element: HTMLInputElement) => {
+  const countRef = (element: HTMLInputElement) => {
     if (element) element.value = '5'
-    this.textbox = element
+    textbox = element
   }
 
-  onCreate = () => {
-    const count = parseInt(this.textbox.value, 10)
+  const onCreate = () => {
+    const count = parseInt(textbox.value, 10)
     parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
   }
 
-  onCancel = () => {
+  const onCancel = () => {
     parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
   }
 
@@ -25,9 +25,9 @@ class App extends React.Component {
     return <div>
       <img src={require('./logo.svg')} />
       <h2>Rectangle Creator</h2>
-      <p>Count: <input ref={this.countRef} /></p>
-      <button id="create" onClick={this.onCreate}>Create</button>
-      <button onClick={this.onCancel}>Cancel</button>
+      <p>Count: <input ref={countRef} /></p>
+      <button id="create" onClick={onCreate}>Create</button>
+      <button onClick={onCancel}>Cancel</button>
     </div>
   }
 }
