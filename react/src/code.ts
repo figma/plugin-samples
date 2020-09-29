1ef31a0181b1a7394433dee5166c943b8d2dc2e8
Robbie Cook <robbieck7@gmail.com>
2020-09-29 14:09:43 +1300

    Adding plugin typing files

--- react/src/code.ts
+++ react/src/code.ts
@@ -1,20 +1,22 @@
-figma.showUI(__html__)
+/// <reference path="../node_modules/@figma/plugin-typings/index.d.ts" />
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = []
+figma.showUI(__html__);
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === "create-rectangles") {
+    const nodes = [];
 
     for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle()
-      rect.x = i * 150
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}]
-      figma.currentPage.appendChild(rect)
-      nodes.push(rect)
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
     }
 
-    figma.currentPage.selection = nodes
-    figma.viewport.scrollAndZoomIntoView(nodes)
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
-  figma.closePlugin()
-}
+  figma.closePlugin();
+};
