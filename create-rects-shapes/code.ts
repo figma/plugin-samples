6ea777485f2c800ea2d138155d01071df3543adf
Bersasbel Tadesse <bersasbeltadesse@Bersasbel-Tadesses-computer.local>
2021-08-23 22:29:38 -0700

    remove UI

--- create-rects-shapes/code.ts
+++ create-rects-shapes/code.ts
@@ -1,91 +1,73 @@
+// runs this code if the plugin is run in Figma
+
 if (figma.editorType === 'figma') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rectangles on the screen.
-
+  // This plugin creates 5 rectangles on the screen.
+  const numberOfRectangles = 5
+  
   // This file holds the main code for the plugins. It has access to the *document*.
-  // You can access browser APIs in the <script> tag inside "ui.html" which has a
-  // full browser environment (see documentation).
-
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < msg.count; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+  // You can access browser APIs such as the network by creating a UI which contains
+  // a full browser environment (see documentation).
+  
+  const nodes: SceneNode[] = [];
+  for (let i = 0; i < numberOfRectangles; i++) {
+    const rect = figma.createRectangle();
+    rect.x = i * 150;
+    rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    figma.currentPage.appendChild(rect);
+    nodes.push(rect);
+  }
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
+  
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+  
+  }
+  
+  // runs this code if the plugin is run in FigJam
+  
+  if (figma.editorType === 'figjam') {
+  // This plugin creates 5 shapes with text on the screen, and adds a connector in between each one.
+  const numberOfShapes = 5
+  
+  // This file holds the main code for the plugin. It has access to the *document*.
+  // You can access browser APIs such as the network by creating a UI which contains
+  // a full browser environment (see documentation).
+  
+  const nodes: SceneNode[] = [];
+  for (let i = 0; i < numberOfShapes; i++) {
+    const shape = figma.createShapeWithText();
+    shape.shapeType = 'ROUNDED_RECTANGLE'
+    // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
+    shape.x = i * 400;
+    shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+    figma.currentPage.appendChild(shape);
+    nodes.push(shape);
   };
-};
-
-if (figma.editorType === 'figjam') {
-  // This plugin will open a window to prompt the user to enter a number, and
-  // it will then create that many rounded rectangle nodes, and will add a connector node in between each node.
-
-  // This file holds the main code for the plugins. It has access to the *document*.
-  // You can access browser APIs in the <script> tag inside "ui.html" which has a
-  // full browser environment (see documentation).
-
-  // This shows the HTML page in "ui.html".
-  figma.showUI(__html__);
-
-  // Calls to "parent.postMessage" from within the HTML page will trigger this
-  // callback. The callback will be passed the "pluginMessage" property of the
-  // posted message.
-  figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-      const numberOfShapes = msg.count; 
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < numberOfShapes; i++) {
-        const shape = figma.createShapeWithText();
-        shape.shapeType = 'ROUNDED_RECTANGLE'
-        // You can set shapeType to one of: 'SQUARE' | 'ELLIPSE' | 'ROUNDED_RECTANGLE' | 'DIAMOND' | 'TRIANGLE_UP' | 'TRIANGLE_DOWN' | 'PARALLELOGRAM_RIGHT' | 'PARALLELOGRAM_LEFT'
-        shape.x = i * 400;
-        shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-        figma.currentPage.appendChild(shape);
-        nodes.push(shape);
-      };
-      
-      for (let i = 0; i < (numberOfShapes - 1); i++) {
-        const connector = figma.createConnector();
-        const strokeWeight = {...connector.strokeWeight};
-        connector.strokeWeight = 8
-        
-        connector.connectorStart = {
-          endpointNodeId: nodes[i].id,
-          magnet: 'AUTO',
-        }; 
-        
-        connector.connectorEnd = {
-          endpointNodeId: nodes[i+1].id,
-          magnet: 'AUTO',
-        };
-      };
-      
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+  
+  for (let i = 0; i < (numberOfShapes - 1); i++) {
+    const connector = figma.createConnector();
+    const strokeWeight = {...connector.strokeWeight};
+    connector.strokeWeight = 8
+    
+    connector.connectorStart = {
+      endpointNodeId: nodes[i].id,
+      magnet: 'AUTO',
+    }; 
+    
+    connector.connectorEnd = {
+      endpointNodeId: nodes[i+1].id,
+      magnet: 'AUTO',
+    };
   };
-};
+  
+  figma.currentPage.selection = nodes;
+  figma.viewport.scrollAndZoomIntoView(nodes);
+  
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+  
+  }
+  
