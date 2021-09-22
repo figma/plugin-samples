2e7c887adb6a9eba059286a4db64c19b961af10b
Jonas Sicking <jonas@figma.com>
2021-09-22 15:06:16 -0700

    Use setError to improve validation and error messages

--- resizer/code.ts
+++ resizer/code.ts
@@ -1,5 +1,10 @@
 // The 'input' event listens for text change in the Quick Actions box after a plugin is 'Tabbed' into.
 figma.parameters.on('input', ({query, key, result}: ParameterInputEvent) => {
+  if (figma.currentPage.selection.length === 0) {
+    result.setError('Please select one or mode nodes first')
+    return
+  }
+
   switch (key) {
     case 'width':
       const widthSizes = ['640', '800', '960', '1024', '1280']
@@ -9,8 +14,17 @@ figma.parameters.on('input', ({query, key, result}: ParameterInputEvent) => {
       const heightSizes = ['480', '600', '720', '768', '960']
       result.setSuggestions(heightSizes.filter(s => s.includes(query)))
       break
-    case 'scale': // Freeform parameter that doesn't need suggestions.
-      result.setSuggestions([])
+    case 'scale':
+      // Check that the input is a valid number
+      if (query === '') {
+        result.setSuggestions([])
+      } else if (!Number.isFinite(Number(query))) {
+        result.setError("Please enter a numberic value")
+      } else if (Number(query) <= 0) {
+        result.setError("Must be larger than 0")
+      } else {
+        result.setSuggestions([query])
+      }
       break
     default:
       return
@@ -18,35 +32,32 @@ figma.parameters.on('input', ({query, key, result}: ParameterInputEvent) => {
 })
 
 // When the user presses Enter after inputting all parameters, the 'run' event is fired.
-figma.on('run', ({parameters}: RunEvent) => {
-  if (parameters) {
-    startPluginWithParameters(parameters)
-  }
-})
-
-function startPluginWithParameters(parameters: ParameterValues) {
-  const selection = figma.currentPage.selection
-
-  // Check if we're doing relative or absolute resize.
-  if (parameters['scale']) {
-    const scale = parseFloat(parameters['scale'])
-
-    for (let i = 0; i < selection.length; i++) {
-      const node = selection[i]
-      if ('rescale' in node) {
-        node.rescale(scale)
-      }
-    }
-  } else if (parameters['width'] && parameters['height']) {
-    const width = parseInt(parameters['width'])
-    const height = parseInt(parameters['height'])
-
-    for (let i = 0; i < selection.length; i++) {
-      const node = selection[i]
-      if ('resize' in node) {
-        node.resize(width, height)
-      }
-    }
+figma.on('run', ({command, parameters}: RunEvent) => {
+  if (command == 'relative') {
+    resizeRelative(parameters)
+  } else {
+    resizeAbsolute(parameters)
   }
   figma.closePlugin()
+})
+
+function resizeRelative(parameters: ParameterValues) {
+  const scale = parseFloat(parameters.scale)
+
+  for (const node of figma.currentPage.selection) {
+    if ('rescale' in node) {
+      node.rescale(scale)
+    }
+  }
+}
+
+function resizeAbsolute(parameters: ParameterValues) {
+  const width = parseInt(parameters.width)
+  const height = parseInt(parameters.height)
+
+  for (const node of figma.currentPage.selection) {
+    if ('resize' in node) {
+      node.resize(width, height)
+    }
+  }
 }
