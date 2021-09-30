59db36d64f4c9561b09ed56795f3cf539804f21e
Jackie Chui <jchui@figma.com>
2021-09-30 10:55:38 -0700

    Moved onmessage handler out of parameters.on

--- capital/code.ts
+++ capital/code.ts
@@ -1,6 +1,6 @@
 // This plugin fetches a list of countries from an external API and shows the capital of the selected country
 
-let countries = [];
+let countries, resultObj;
 
 // Create an invisible iframe UI to use network API's
 figma.showUI(
@@ -14,27 +14,28 @@ figma.showUI(
   { visible: false }
 );
 
+figma.ui.onmessage = (json) => {
+  countries = json.map((country, index) => {
+    return {
+      name: country.name,
+      data: {
+        index: index,
+        name: country.name,
+        capital: country.capital,
+      },
+    };
+  });
+  // Set the suggestions after the a message has been received from the iframe
+  resultObj.setSuggestions(countries);
+};
+
 figma.parameters.on('input', ({ key, query, result }: ParameterInputEvent) => {
+  resultObj = result;
   // When fetching data from an external source, it is recommended to show a relevant loading message
   result.setLoadingMessage('Loading countries...');
 
-  // Set the suggestions after the a message has been received from the iframe
-  figma.ui.onmessage = (json) => {
-    countries = json.map((country, index) => {
-      return {
-        name: country.name,
-        data: {
-          index: index,
-          name: country.name,
-          capital: country.capital,
-        },
-      };
-    });
-    result.setSuggestions(countries);
-  };
-
   // Perform filtering on the suggestions if necessary
-  if (query !== '') {
+  if (query !== '' && countries) {
     result.setSuggestions(
       countries.filter((country) =>
         country.name.toLowerCase().includes(query.toLowerCase())
