3cbe824785ee0950e73547b2236ab1850c0526a6
Jake Albaugh <jalbaugh@figma.com>
2022-10-13 17:15:36 -0500

    better style

--- document-change/code.ts
+++ document-change/code.ts
@@ -1,5 +1,19 @@
-figma.showUI(__html__, { height: 600, width: 400 });
+figma.showUI(__html__, { height: 600, width: 600 });
 
 figma.on("documentchange", (event) => {
-  figma.ui.postMessage(event, { origin: "*" });
+  const messages = event.documentChanges.map(documentChangeAsString);
+  figma.ui.postMessage(messages, { origin: "*" });
 });
+
+function documentChangeAsString(change) {
+  const { origin, type } = change;
+  const list = [origin, type];
+  if (type === "PROPERTY_CHANGE") {
+    list.push(change.node.type, change.properties.join(", "));
+  } else if (type === "STYLE_CHANGE") {
+    list.push(change.style.name, change.properties.join(", "));
+  } else {
+    list.push(change.node.type);
+  }
+  return list.join(" ");
+}
