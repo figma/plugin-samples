b8f2bd1ba9d3734acd1aee4adcb14ffe0ed45d81
Jake Albaugh <jalbaugh@figma.com>
2022-12-05 15:13:19 -0600

    simplifying

--- text-review/code.js
+++ text-review/code.js
@@ -1,22 +1,14 @@
-const find = "figma";
-const suggestion = "✨Figma✨";
-const color = "GREEN";
-
 if (figma.command === "textreview") {
-  figma.on("textreview", async ({ text }) => {
-    const chunks = text
-      .replace(new RegExp(suggestion, "g"), suggestion.replace(/./g, "x"))
-      .toLowerCase()
-      .split(find);
-    let end = 0;
-    return chunks
-      .map((string, i) => {
-        if (i === chunks.length - 1) return null;
-        const start = end + string.length;
-        end = start + find.length;
-        return { start, end, suggestions: [suggestion], color };
-      })
-      .filter(Boolean);
+  figma.on("textreview", ({ text }) => {
+    // Replacing "✨Figma✨" with "xxxxxxx" so we don't highlight "✨Figma✨"
+    const cleanText = text.replace(/✨Figma✨/g, "xxxxxxx");
+    const matches = Array.from(cleanText.matchAll(/figma/gi));
+    return matches.map((match) => ({
+      start: match.index,
+      end: match.index + match[0].length,
+      suggestions: ["✨Figma✨"],
+      color: "GREEN",
+    }));
   });
 } else {
   figma.notify("I am running like any other plugin!");
