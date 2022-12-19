e33b28b55d63d49767005342d115ebf2e6d84059
Ibrahim Memon <106296670+ibrahimmemonn@users.noreply.github.com>
2022-12-19 12:54:31 +0500

    Code Cleaning

--- icon-drag-and-drop-hosted/code.ts
+++ icon-drag-and-drop-hosted/code.ts
@@ -6,11 +6,9 @@ figma.on('drop', (event: DropEvent) => {
   
   if (items.length > 0 && items[0].type === 'image/svg+xml') {
     const data = items[0].data
-
     const newNode = figma.createNodeFromSvg(data);
     newNode.x = event.absoluteX;
     newNode.y = event.absoluteY;
-
     figma.currentPage.selection = [newNode];
   }
   
