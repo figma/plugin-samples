e1e12a21fd822ed40b34fedb14bbbfc404833096
svarainis450 <svarainis450@gmail.com>
2023-01-28 11:22:37 +0000

    chore(esbuild-react): upgrade vite to v4 and resolve other vulnerabilities

--- esbuild-react/vite.config.ts
+++ esbuild-react/vite.config.ts
@@ -1,23 +1,17 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./ui-src",
-  plugins: [reactRefresh(), viteSingleFile()],
+  plugins: [react(), viteSingleFile()],
   build: {
     target: "esnext",
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
     outDir: "../dist",
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => "everything.js",
-      },
-    },
+    reportCompressedSize: true
   },
 });
