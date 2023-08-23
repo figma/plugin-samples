c963a336b35587b07edc541dd4a546b3892459f7
yinm <yinm.cminor9@gmail.com>
2023-08-23 21:56:07 +0900

    Widen array type for push method

--- document-change/code.ts
+++ document-change/code.ts
@@ -7,7 +7,7 @@ figma.on("documentchange", (event) => {
 
 function documentChangeAsString(change: DocumentChange) {
   const { origin, type } = change;
-  const list = [origin, type];
+  const list: string[] = [origin, type];
   if (type === "PROPERTY_CHANGE") {
     list.push(change.node.type, change.properties.join(", "));
   } else if (type === "STYLE_PROPERTY_CHANGE") {
