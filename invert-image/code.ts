06f4412cafebf606b9d56cbeef1091c891ff5753
Akbar Mirza <akbarbmirza@gmail.com>
2024-01-19 17:33:25 -0500

    Update invert-image for Dynamic Page Loading

--- invert-image/code.ts
+++ invert-image/code.ts
@@ -1,4 +1,4 @@
-async function invertPaint(paint) {
+async function invertPaint(paint: Paint) {
   // Only invert the color for images (but you could do it
   // for solid paints and gradients if you wanted)
   if (paint.type === 'IMAGE') {
@@ -21,45 +21,34 @@ async function invertPaint(paint) {
     figma.ui.postMessage(bytes)
 
     // Wait for the worker's response
-    const newBytes: Uint8Array = await new Promise((resolve, reject) => {
+    const newBytes: Uint8Array = await new Promise((resolve) => {
       figma.ui.onmessage = value => resolve(value as Uint8Array)
     })
 
     // Create a new paint for the new image. Uploading the image will give us
     // an image hash.
-    const newPaint = JSON.parse(JSON.stringify(paint))
+    const newPaint = {...paint}
     newPaint.imageHash = figma.createImage(newBytes).hash
     return newPaint
   }
   return paint
 }
 
-async function invertIfApplicable(node) {
+async function invertIfApplicable(node: SceneNode) {
   // Look for fills on node types that have fills.
   // An alternative would be to do `if ('fills' in node) { ... }
-  switch (node.type) {
-    case 'RECTANGLE':
-    case 'ELLIPSE':
-    case 'POLYGON':
-    case 'STAR':
-    case 'VECTOR':
-    case 'TEXT': {
-      // Create a new array of fills, because we can't directly modify the old one
-      const newFills = []
-      for (const paint of node.fills) {
-        newFills.push(await invertPaint(paint))
-      }
-      node.fills = newFills
-      break
+  if ('fills' in node) {
+    // Create a new array of fills, because we can't directly modify the old one
+    const newFills = []
+    for (const paint of node.fills as Paint[]) {
+      newFills.push(await invertPaint(paint))
     }
-
-    default: {
-      // not supported, silently do nothing
-    }
+    node.fills = newFills
   }
 }
 
 // This plugin looks at all the currently selected nodes and inverts the colors
 // in their image, if they use an image paint.
-Promise.all(figma.currentPage.selection.map(selected => invertIfApplicable(selected)))
-       .then(() => figma.closePlugin())
+Promise.all(
+  figma.currentPage.selection.map(selected => invertIfApplicable(selected))
+).then(() => figma.closePlugin()).catch(() => {console.log("This operation failed")});
