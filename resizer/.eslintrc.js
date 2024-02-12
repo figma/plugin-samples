ba0d5b99054e5c37d4620c3ca40316d46b234a72
Akbar Mirza <akbarbmirza@gmail.com>
2024-02-12 16:17:36 -0500

    Reference NPM Package for ESLint Plugin

--- resizer/.eslintrc.js
+++ resizer/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:@figma/figma-plugins/dynamic-page",
+    "plugin:@figma/figma-plugins/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
